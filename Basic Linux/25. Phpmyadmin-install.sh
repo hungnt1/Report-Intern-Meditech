a4961e1b32d07a970f5253c1fc99d7f105e5c276
nguyenhungsync <nguyenhungsync@gmail.com>
2018-12-29 15:51:27 +0700

    done

---
+++ Basic Linux/25. Phpmyadmin-install.sh
@@ -0,0 +1,11 @@
+yum -y install epel-release
+yum -y install httpd
+systemctl start httpd 	
+systemctl enable httpd 
+firewall-cmd --add-service=http --permanent 
+firewall-cmd --reload 
+yum -y install php php-mbstring php-pear
+yum -y install php php-mbstring php-pear
+yum --enablerepo=epel -y install phpMyAdmin php-mysql php-mcrypt
+sed -i -e "s#127.0.0.1 192.168.20.0/24#127.0.0.1 10.10.0.0/24 #g" /etc/httpd/conf.d/phpMyAdmin.conf
+systemctl restart httpd 
