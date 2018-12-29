fa065a846966fdd2909395539995293e2d52a2a3
nguyenhungsync <nguyenhungsync@gmail.com>
2018-12-29 15:51:50 +0700

    Update 25. Phpmyadmin-install.sh

--- Basic Linux/25. Phpmyadmin-install.sh
+++ Basic Linux/25. Phpmyadmin-install.sh
@@ -7,5 +7,5 @@ firewall-cmd --reload
 yum -y install php php-mbstring php-pear
 yum -y install php php-mbstring php-pear
 yum --enablerepo=epel -y install phpMyAdmin php-mysql php-mcrypt
-sed -i -e "s#127.0.0.1 192.168.20.0/24#127.0.0.1 10.10.0.0/24 #g" /etc/httpd/conf.d/phpMyAdmin.conf
+sed -i -e "s#127.0.0.1#127.0.0.1 10.10.0.0/24 #g" /etc/httpd/conf.d/phpMyAdmin.conf
 systemctl restart httpd 
